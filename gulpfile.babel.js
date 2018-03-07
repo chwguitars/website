e5fdc3ad128ea242487347af7a55ea2409b8974a
Joe Purdy <hello@joepurdy.io>
2018-03-06 21:26:06 -0800

    Update backend

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,126 +1,98 @@
-'use strict'
+import gulp from "gulp";
+import cp from "child_process";
+import gutil from "gulp-util";
+import postcss from "gulp-postcss";
+import cssImport from "postcss-import";
+import cssnext from "postcss-cssnext";
+import BrowserSync from "browser-sync";
+import webpack from "webpack";
+import webpackConfig from "./webpack.conf";
+import svgstore from "gulp-svgstore";
+import svgmin from "gulp-svgmin";
+import inject from "gulp-inject";
+import cssnano from "cssnano";
 
-import gulp from 'gulp'
-import del from 'del'
-import runSequence from 'run-sequence'
-import gulpLoadPlugins from 'gulp-load-plugins'
-import { spawn } from "child_process"
-import tildeImporter from 'node-sass-tilde-importer'
-import webpack from 'webpack-stream'
+const browserSync = BrowserSync.create();
+const hugoBin = `hugo`;
+const defaultArgs = ["-d", "../dist", "-s", "site"];
 
-const $ = gulpLoadPlugins()
-const browserSync = require('browser-sync').create()
-const isProduction = process.env.NODE_ENV === 'production'
-
-const onError = (err) => {
-    console.log(err)
+if (process.env.DEBUG) {
+  defaultArgs.unshift("--debug")
 }
 
-// --
+gulp.task("hugo", (cb) => buildSite(cb));
+gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
+gulp.task("build", ["css", "js", "cms-assets", "hugo"]);
+gulp.task("build-preview", ["css", "js", "cms-assets", "hugo-preview"]);
 
-gulp.task('server', ['build'], () => {
-    gulp.start('init-watch')
-    $.watch(['archetypes/**/*', 'data/**/*', 'content/**/*', 'layouts/**/*', 'static/**/*', 'config.toml'], () => gulp.start('hugo'))
-});
-
-gulp.task('server:with-drafts', ['build-preview'], () => {
-    gulp.start('init-watch')
-    $.watch(['archetypes/**/*', 'data/**/*', 'content/**/*', 'layouts/**/*', 'static/**/*', 'config.toml'], () => gulp.start('hugo-preview'))
-});
-
-gulp.task('init-watch', () => {
-    browserSync.init({
-        server: {
-            baseDir: 'public'
-        },
-        open: false
-    })
-    $.watch('src/sass/**/*.scss', () => gulp.start('sass'))
-    $.watch('src/js/**/*.js', () => gulp.start('js-watch'))
-    $.watch('src/images/**/*', () => gulp.start('images'))  
-})
-
-gulp.task('build', () => {
-    runSequence(['sass', 'js', 'fonts', 'images', 'pub-delete'], 'hugo')
-})
-
-gulp.task('build-preview', () => {
-    runSequence(['sass', 'js', 'fonts', 'images', 'pub-delete'], 'hugo-preview')
-})
-
-gulp.task('hugo', (cb) => {
-    return spawn('hugo', { stdio: 'inherit' }).on('close', (code) => {
-        browserSync.reload()
-        cb()
-    })
-})
-
-gulp.task('hugo-preview', (cb) => {
-    return spawn('hugo', ['--buildDrafts', '--buildFuture'], { stdio: 'inherit' }).on('close', (code) => {
-        browserSync.reload()
-        cb()
-    })
-})
-
-// --
-
-gulp.task('sass', () => {
-    return gulp.src([
-        'src/sass/app.scss'
-    ])
-    .pipe($.plumber({ errorHandler: onError }))
-    .pipe($.print())
-    .pipe($.sassLint())
-    .pipe($.sassLint.format())
-    .pipe($.sass({ precision: 5, importer: tildeImporter }))
-    .pipe($.autoprefixer(['ie >= 10', 'last 2 versions']))
-    .pipe($.if(isProduction, $.cssnano({ discardUnused: false, minifyFontValues: false })))
-    .pipe($.size({ gzip: true, showFiles: true }))
-    .pipe(gulp.dest('static/css'))
+gulp.task("css", () => (
+  gulp.src("./src/css/*.css")
+    .pipe(postcss([
+      cssImport({from: "./src/css/main.css"}),
+      cssnext(),
+      cssnano(),
+    ]))
+    .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
-})
+));
 
-gulp.task('js-watch', ['js'], (cb) => {
+gulp.task("cms-assets", () => (
+  gulp.src("./node_modules/netlify-cms/dist/*.{woff,eot,woff2,ttf,svg,png}")
+    .pipe(gulp.dest("./dist/css"))
+))
+
+gulp.task("js", (cb) => {
+  const myConfig = Object.assign({}, webpackConfig);
+
+  webpack(myConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString({
+      colors: true,
+      progress: true
+    }));
     browserSync.reload();
     cb();
+  });
 });
 
-gulp.task('js', () => {
-    return gulp.src([
-        'src/js/app.js'
-    ])
-    .pipe(webpack())
-    .pipe($.plumber({ errorHandler: onError }))
-    .pipe($.print())
-    .pipe($.babel())
-    .pipe($.concat('app.js'))
-    .pipe($.if(isProduction, $.uglify()))
-    .pipe($.size({ gzip: true, showFiles: true }))
-    .pipe(gulp.dest('static/js'))
-})
+gulp.task("svg", () => {
+  const svgs = gulp
+    .src("site/static/images/icons-*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({inlineSvg: true}));
 
-gulp.task('fonts', () => {
-    return gulp.src('src/fonts/**/*.{woff,woff2}')
-        .pipe(gulp.dest('static/fonts'));
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp
+    .src("site/layouts/partials/svg.html")
+    .pipe(inject(svgs, {transform: fileContents}))
+    .pipe(gulp.dest("site/layouts/partials/"));
 });
 
-gulp.task('images', () => {
-    return gulp.src('src/images/**/*.{png,jpg,jpeg,gif,svg,webp,ico}')
-        .pipe($.newer('static/images'))
-        .pipe($.print())
-        .pipe($.imagemin())
-        .pipe(gulp.dest('static/images'));
+gulp.task("server", ["hugo", "css", "cms-assets", "js", "svg"], () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/css/**/*.css", ["css"]);
+  gulp.watch("./site/static/images/icons-*.svg", ["svg"]);
+  gulp.watch("./site/**/*", ["hugo"]);
 });
 
-gulp.task('cms-delete', () => {
-    return del(['static/admin'], { dot: true })
-})
+function buildSite(cb, options) {
+  const args = options ? defaultArgs.concat(options) : defaultArgs;
 
-gulp.task('pub-delete', () => {
-    return del(['public/**', '!public'], {
-      // dryRun: true,
-      dot: true
-    }).then(paths => {
-      console.log('Files and folders deleted:\n', paths.join('\n'), '\nTotal Files Deleted: ' + paths.length + '\n');
-    })
-})
+  return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
+    if (code === 0) {
+      browserSync.reload("notify:false");
+      cb();
+    } else {
+      browserSync.notify("Hugo build failed :(");
+      cb("Hugo build failed");
+    }
+  });
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 import 'bootstrap';
-import { setHours } from './helpers';
+import { setHours } from './contact';
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
 
 if (window.location.pathname === "/contact/") {
   setHours();
